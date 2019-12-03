4722c2cc8c88b0695c7543772312b3d094cea640
swethapoloju <sulochanapoloju16@gmail.com>
2019-12-03 18:34:27 +0530

    project
    project

---
+++ src/js/views/base.js
@@ -0,0 +1,28 @@
+export const  elements ={
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchRelList: document.querySelector('.results__list'),
+    searchResult: document.querySelector('.results'),
+    searchresPage: document.querySelector('.results__pages')
+
+};
+export const elementString ={
+    loader:'loader'
+};
+export const renderLoader =parent =>{
+    const loader =`
+    <div class=${elementString.loader}>
+    <svg>
+        <use href="img/icons.svg#icon-cw"</use>
+    </svg>
+    </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin',loader);
+
+}
+export const clearLoader =()=>{
+    const loader =document.querySelector(`.${elementString.loader}`);
+    if(loader){
+        loader.parentElement.removeChild(loader);
+    }
+};
